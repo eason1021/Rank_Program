54d99760688a511774b45d985a2cfca99c7e966a
eason1021 <45169621+eason1021@users.noreply.github.com>
2022-11-04 14:44:58 +0800

    Add files via upload

--- MSE_Rank.py
+++ MSE_Rank.py
@@ -1,18 +1,28 @@
 import numpy as np
 import pandas as pd
 import os
+import sys
 
-path = os.path.dirname(os.path.realpath(__file__))
+
+#Read Excel file in the same folder
+if getattr(sys, 'frozen', False):
+    path = os.path.dirname(os.path.abspath(sys.executable))
+elif __file__:
+    path = os.path.dirname(os.path.abspath(__file__))
+print(path)
 for i in os.listdir(path):
     if i[-4:] == 'xlsx':
         data_path = path +'/' + i
 df = pd.read_excel(data_path, sheet_name= None)
 
+#Check the number of datasheets & column's name
 group_num = len(df.keys())
 coulumn_name = df.get(list(df.keys())[0]).columns
 group_num_list = []
 switch = False
 
+#Move the information from datasheets into single 2D array
+#Record the number of each datasheet, enable program to accommodate the different number of groups.
 for i in df.keys():
     temp_list = df.get(i).to_numpy()
     group_num_list.append(temp_list.shape[0])
@@ -23,9 +33,11 @@ for i in df.keys():
     else:
         original_list = np.row_stack((original_list, temp_list))
 
+#Create new array to do the S-shape sort
 original_list = original_list.astype(str)
 new_list = np.zeros((original_list.shape[0], original_list.shape[1])).astype(str)
 
+#Record the maximum number from all datasheets
 max_num = max(group_num_list)
 count = 0
 
@@ -72,4 +84,5 @@ for i in range(max_num):
                 pass
 
 new_df = pd.DataFrame(new_list, columns=coulumn_name)
-new_df.to_excel('/workspaces/Code_Garden/cs_class/Test4.xlsx', index=None)
+output_path = path + "/" + 'New_Rank.xlsx'
+new_df.to_excel(output_path, index=None)
